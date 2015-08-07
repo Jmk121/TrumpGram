18bdfe0937b336a7d1801066cca865971b13c1db
jmk121 <jmk121@Ross-Laptop.local>
2015-08-07 09:18:57 -0400

    Initial Commit

---
+++ TrumpGram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrumpGram
+//
+//  Created by jmk121 on 8/7/15.
+//  Copyright (c) 2015 jmk121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
